456921fc24090ddf4072d5c307f51de0c76d5b9f
Guilherme Alves <Guilhermealves53139@gmail.com>
2024-07-17 23:55:46 -0300

    Criando tela crud frontend web com relacionamento parte 2

---
+++ app/(main)/pages/perfil-usuario/page.tsx
@@ -0,0 +1,343 @@
+/* eslint-disable @next/next/no-img-element */
+'use client';
+import { Button } from 'primereact/button';
+import { Column } from 'primereact/column';
+import { DataTable } from 'primereact/datatable';
+import { Dialog } from 'primereact/dialog';
+import { FileUpload } from 'primereact/fileupload';
+import { InputText } from 'primereact/inputtext';
+import { Toast } from 'primereact/toast';
+import { Toolbar } from 'primereact/toolbar';
+import { classNames } from 'primereact/utils';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
+import { Projeto } from '@/types';
+import { PerfilUsuarioService } from '@/service/PerfilUsuarioService';
+
+
+const PerfilUsuario = () => {
+    let perfilUsuarioVazio: Projeto.PerfilUsuario = {
+        id: 0,
+        perfil: {descricao: ''},
+        usuario: {nome:'',login:'',senha:'',email:''}       
+    };
+
+    const [perfisUsuario, setPerfilsUsuario] = useState<Projeto.PerfilUsuario[] | null>(null);
+    const [perfilUsuarioDialog, setPerfilUsuarioDialog] = useState(false);
+    const [deletePerfilUsuarioDialog, setDeletePerfilUsuarioDialog] = useState(false);
+    const [deletePerfilsUsuarioDialog, setDeletePerfilsUsuarioDialog] = useState(false);
+    const [perfilUsuario, setPerfilUsuario] = useState<Projeto.PerfilUsuario>(perfilUsuarioVazio);
+    const [selectedPerfilsUsuario, setSelectedPerfilsUsuario] = useState<Projeto.PerfilUsuario[]>([]);
+    const [submitted, setSubmitted] = useState(false);
+    const [globalFilter, setGlobalFilter] = useState('');
+    const toast = useRef<Toast>(null);
+    const dt = useRef<DataTable<any>>(null);
+    const perfilUsuarioService = useMemo(() => new PerfilUsuarioService(), []);
+
+    useEffect(() => {
+        if (!perfisUsuario) {
+            perfilUsuarioService.listarTodos()
+                .then((response) => {
+                    console.log(response.data);
+                    setPerfilsUsuario(response.data)
+                }).catch((error) => {
+                    console.log(error);
+                })
+        }
+    }, [perfilUsuarioService, perfisUsuario]);
+
+
+    const openNew = () => {
+        setPerfilUsuario(perfilUsuarioVazio);
+        setSubmitted(false);
+        setPerfilUsuarioDialog(true);
+    };
+
+    const hideDialog = () => {
+        setSubmitted(false);
+        setPerfilUsuarioDialog(false);
+    };
+
+    const hideDeletePerfilDialog = () => {
+        setDeletePerfilUsuarioDialog(false);
+    };
+
+    const hideDeletePerfilsDialog = () => {
+        setDeletePerfilsUsuarioDialog(false);
+    };
+
+    const savePerfilUsuario = () => {
+        setSubmitted(true);
+
+        if (!perfilUsuario.id) {
+            perfilUsuarioService.inserir(perfilUsuario)
+                .then((response) => {
+                    setPerfilUsuarioDialog(false);
+                    setPerfilUsuario(perfilUsuarioVazio);
+                    setPerfilsUsuario(null);
+                    toast.current?.show({
+                        severity: 'info',
+                        summary: 'Successo!',
+                        detail: 'Perfil Usuário cadastrado com sucesso!'
+                    });
+                }).catch((error) => {
+                    console.log(error.data.message);
+                    toast.current?.show({
+                        severity: 'error',
+                        summary: 'Erro!',
+                        detail: 'Erro ao salvar!' + error.data.message
+                    })
+                });
+        } else {
+            perfilUsuarioService.alterar(perfilUsuario)
+                .then((response) => {
+                    setPerfilUsuarioDialog(false);
+                    setPerfilUsuario(perfilUsuarioVazio);
+                    setPerfilsUsuario(null);
+                    toast.current?.show({
+                        severity: 'info',
+                        summary: 'Successo!',
+                        detail: 'Perfil Usuário alterado com sucesso!'
+                    });
+                }).catch((error) => {
+                    console.log(error.data.message);
+                    toast.current?.show({
+                        severity: 'error',
+                        summary: 'Error!',
+                        detail: 'Erro ao alterar!' + error.data.message
+                    })
+                })
+        }
+
+    }
+
+    const editPerfilUsuario = (perfil: Projeto.PerfilUsuario) => {
+        setPerfilUsuario({ ...perfil });
+        setPerfilUsuarioDialog(true);
+    };
+
+    const confirmDeletePerfilUsuario = (perfilUsuario: Projeto.PerfilUsuario) => {
+        setPerfilUsuario(perfilUsuario);
+        setDeletePerfilUsuarioDialog(true);
+    };
+
+    const deletePerfilUsuario = () => {
+        if (perfilUsuario.id) {
+            perfilUsuarioService.excluir(perfilUsuario.id).then((response) => {
+                setPerfilUsuario(perfilUsuarioVazio);
+                setDeletePerfilUsuarioDialog(false);
+                setPerfilsUsuario(null);
+                toast.current?.show({
+                    severity: 'success',
+                    summary: 'Sucesso!',
+                    detail: 'Perfil Usuário Deletado com Sucesso',
+                    life: 3000
+                });
+            }).catch((error) => {
+                toast.current?.show({
+                    severity: 'error',
+                    summary: 'Erro!',
+                    detail: 'Erro ao Deletadar o Perfil!',
+                    life: 3000
+                });
+            });
+        }
+    };
+
+
+    const exportCSV = () => {
+        dt.current?.exportCSV();
+    };
+
+    const confirmDeleteSelected = () => {
+        setDeletePerfilsDialog(true);
+    };
+
+    const deleteSelectedPerfils = () => {
+
+        Promise.all(selectedPerfils.map(async (_perfil) => {
+            if (_perfil.id) {
+                await perfilService.excluir(_perfil.id);
+            }
+        })).then((response) => {
+            setPerfils(null);
+            setSelectedPerfils(([]));
+            setDeletePerfilsDialog(false);
+            toast.current?.show({
+                severity: 'success',
+                summary: 'Sucesso!',
+                detail: 'Perfils Deletados com Sucesso',
+                life: 3000
+            });
+        }).catch((error) =>{
+            toast.current?.show({
+                severity: 'error',
+                summary: 'Erro!',
+                detail: 'Erro ao Deletadar o Perfils!',
+                life: 3000
+
+        })});
+    };
+
+    const onInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>, name: string) => {
+        const val = (e.target && e.target.value) || '';
+        //JEITO ANTIGO MOSTRANDO ERRO
+        // let _recurso = { ...recurso };
+        // _recurso[`${name}`] = val;
+
+        // setRecurso(_recurso);
+
+        // JEITO CERTO
+        setPerfil(prevPerfil => ({
+             ...prevPerfil,
+              [name]: val,
+        }));
+    };
+
+    const leftToolbarTemplate = () => {
+        return (
+            <React.Fragment>
+                <div className="my-2">
+                    <Button label="Novo" icon="pi pi-plus" severity="success" className=" mr-2" onClick={openNew} />
+                    <Button label="Excluir" icon="pi pi-trash" severity="danger" onClick={confirmDeleteSelected} disabled={!selectedPerfils || !(selectedPerfils as any).length} />
+                </div>
+            </React.Fragment>
+        );
+    };
+
+    const rightToolbarTemplate = () => {
+        return (
+            <React.Fragment>
+                <FileUpload mode="basic" accept="image/*" maxFileSize={1000000} chooseLabel="Import" className="mr-2 inline-block" />
+                <Button label="Export" icon="pi pi-upload" severity="help" onClick={exportCSV} />
+            </React.Fragment>
+        );
+    };
+
+    const idBodyTemplate = (rowData: Projeto.Perfil) => {
+        return (
+            <>
+                <span className="p-column-title">Código</span>
+                {rowData.id}
+            </>
+        );
+    };
+
+    const descricaoBodyTemplate = (rowData: Projeto.Perfil) => {
+        return (
+            <>
+                <span className="p-column-title">Descricao</span>
+                {rowData.descricao}
+            </>
+        );
+    };
+
+    const actionBodyTemplate = (rowData: Projeto.Perfil) => {
+        return (
+            <>
+                <Button icon="pi pi-pencil" rounded severity="success" className="mr-2" onClick={() => editPerfil(rowData)} />
+                <Button icon="pi pi-trash" rounded severity="warning" onClick={() => confirmDeletePerfil(rowData)} />
+            </>
+        );
+    };
+
+    const header = (
+        <div className="flex flex-column md:flex-row md:justify-content-between md:align-items-center">
+            <h5 className="m-0">Gerenciamento de Perfils</h5>
+            <span className="block mt-2 md:mt-0 p-input-icon-left">
+                <i className="pi pi-search" />
+                <InputText type="search" onInput={(e) => setGlobalFilter(e.currentTarget.value)} placeholder="Search..." />
+            </span>
+        </div>
+    );
+
+    const perfilDialogFooter = (
+        <>
+            <Button label="Cancelar" icon="pi pi-times" text onClick={hideDialog} />
+            <Button label="Salvar" icon="pi pi-check" text onClick={savePerfil} />
+        </>
+    );
+    const deletePerfilDialogFooter = (
+        <>
+            <Button label="Não" icon="pi pi-times" text onClick={hideDeletePerfilDialog} />
+            <Button label="Sim" icon="pi pi-check" text onClick={deletePerfil} />
+        </>
+    );
+    const deletePerfilsDialogFooter = (
+        <>
+            <Button label="Não" icon="pi pi-times" text onClick={hideDeletePerfilsDialog} />
+            <Button label="Sim" icon="pi pi-check" text onClick={deleteSelectedPerfils} />
+        </>
+    );
+
+    return (
+        <div className="grid crud-demo">
+            <div className="col-12">
+                <div className="card">
+                    <Toast ref={toast} />
+                    <Toolbar className="mb-4" left={leftToolbarTemplate} right={rightToolbarTemplate}></Toolbar>
+
+                    <DataTable
+                        ref={dt}
+                        value={perfis}
+                        selection={selectedPerfils}
+                        onSelectionChange={(e) => setSelectedPerfils(e.value as any)}
+                        dataKey="id"
+                        paginator
+                        rows={10}
+                        rowsPerPageOptions={[5, 10, 25]}
+                        className="datatable-responsive"
+                        paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
+                        currentPageReportTemplate="Mostrando {first} até {last} de {totalRecords} perfil"
+                        globalFilter={globalFilter}
+                        emptyMessage="Nenhum perfil encontrado."
+                        header={header}
+                        responsiveLayout="scroll"
+                    >
+                        <Column selectionMode="multiple" headerStyle={{ width: '4rem' }}></Column>
+                        <Column field="id" header="Código" sortable body={idBodyTemplate} headerStyle={{ minWidth: '15rem' }}></Column>
+                        <Column field="descricao" header="Descricao" sortable body={descricaoBodyTemplate} headerStyle={{ minWidth: '15rem' }}></Column>
+                        <Column body={actionBodyTemplate} headerStyle={{ minWidth: '10rem' }}></Column>
+                    </DataTable>
+
+                    <Dialog visible={perfilDialog} style={{ width: '450px' }} header="Detalhes de Perfil" modal className="p-fluid" footer={perfilDialogFooter} onHide={hideDialog}>
+
+                        <div className="field">
+                            <label htmlFor="descricao">Descricao</label>
+                            <InputText
+                                id="descricao"
+                                value={perfil.descricao}
+                                onChange={(e) => onInputChange(e, 'descricao')}
+                                required
+                                autoFocus
+                                className={classNames({
+                                    'p-invalid': submitted && !perfil.descricao
+                                })}
+                            />
+                            {submitted && !perfil.descricao && <small className="p-invalid">Perfil é obrigatório.</small>}
+                        </div>
+                    </Dialog>
+
+                    <Dialog visible={deletePerfilDialog} style={{ width: '450px' }} header="Confirmar" modal footer={deletePerfilDialogFooter} onHide={hideDeletePerfilDialog}>
+                        <div className="flex align-items-center justify-content-center">
+                            <i className="pi pi-exclamation-triangle mr-3" style={{ fontSize: '2rem' }} />
+                            {perfil && (
+                                <span>
+                                    Você realmente deseja excluir o perfil <b>{perfil.descricao}</b>?
+                                </span>
+                            )}
+                        </div>
+                    </Dialog>
+
+                    <Dialog visible={deletePerfilsDialog} style={{ width: '450px' }} header="Confirmar" modal footer={deletePerfilsDialogFooter} onHide={hideDeletePerfilsDialog}>
+                        <div className="flex align-items-center justify-content-center">
+                            <i className="pi pi-exclamation-triangle mr-3" style={{ fontSize: '2rem' }} />
+                            {perfil && <span>Você realmente deseja excluir os perfils selecionados?</span>}
+                        </div>
+                    </Dialog>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Perfil;
