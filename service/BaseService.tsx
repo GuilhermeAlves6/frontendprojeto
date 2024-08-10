5ce0919761bec9d28bc143c65d40a83420d609ef
Guilherme Alves <Guilhermealves53139@gmail.com>
2024-08-09 22:31:23 -0300

    USO AXIOS INTERCEPTOR PARA ACESSAR BACKEND COM TOKEN ACCESS - FRONTEND REACT.JS / NEXT.JS

--- service/BaseService.tsx
+++ service/BaseService.tsx
@@ -5,31 +5,53 @@ export const axiosInstance = axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL_API
 })
 
-export class BaseService{
+export class BaseService {
 
     url: string;
 
     constructor(url: string) {
         this.url = url;
+
+        axiosInstance.interceptors.request.use((config) => {
+            const token = localStorage.getItem('TOKEN_APLICACAO_FRONTEND');
+            const authRequestToken = token ? `Bearer ${token}` : '';
+            config.headers['Authorization'] = authRequestToken;
+            return config;
+        },
+            (error) => Promise.reject(error)
+        );
+
+        axiosInstance.interceptors.request.use((response) => {
+            return response;
+        }, async (erro) => {
+            const originalConfig = erro.config;
+            console.log(erro.response.status);
+            if (erro.response.status == 401) {
+                localStorage.status.removeItem('TOKEN_APLICACAO_FRONTEND');
+                window.location.reload();
+            }
+            return Promise.reject(erro);
+        });
+
     }
 
-    listarTodos(){
+    listarTodos() {
         return axiosInstance.get(this.url);
     }
 
-    buscarPorId(id: number){
+    buscarPorId(id: number) {
         return axiosInstance.get(this.url + "/" + id);
     }
 
-    inserir(objeto: any){
+    inserir(objeto: any) {
         return axiosInstance.post(this.url, objeto);
     }
 
-    alterar(objeto: any){
+    alterar(objeto: any) {
         return axiosInstance.put(this.url, objeto);
     }
 
-    excluir(id: number){
+    excluir(id: number) {
         return axiosInstance.delete(this.url + "/" + id);
     }
 }    
